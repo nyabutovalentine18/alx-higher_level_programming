2fc931b1d8c0c240ef61ec729d9ea6bf96ab98f8
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-11 23:27:26 -0700

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    from hidden_4 import
     for j in dir(hidden_4):
         if names[:2] != "__":
             print(names)
