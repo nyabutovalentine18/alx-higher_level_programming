d7ab905b03e115450b87285c810d42e7b68a99d9
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-14 13:17:46 -0700

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = len(my_list)
+    list_output = []
+    for i in range(new_list):
+        if my_list[i] % 2 == 0:
+            list_output.append(True)
+        else:
+           list_output.append(False)
+    return list_output
