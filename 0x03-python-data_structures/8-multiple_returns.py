ffd1a9a1ef2c58c6f25bc192cf572044307e6259
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-14 06:28:20 -0700

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #1/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return "None"
+        return (0, None)
     else:
-        print("{:1}".format(sentence))
+        return (len(sentence), sentence[0])
