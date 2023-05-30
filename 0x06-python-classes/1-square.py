e291dbdf1bfbfff9075ba5587df0d54f5cf4fc08
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-30 06:47:27 -0700

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module defines a square class"""
+
+class Square:
+   """Defines a square"""
+
+    def __init__(self, size):
+        """The function initializes a private attribute size"""
+
+        self.__size = size
