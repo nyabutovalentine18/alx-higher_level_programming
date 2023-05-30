4b98d706bfbe2fdfd2f7e7135b7822c3efb6035a
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-30 07:13:49 -0700

    a class Square that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,13 @@ The size attribute is only positive numbers"""
 class Square:
     """The class defines a square and checks the size attribute"""
 
-    def __inti__(self, size=0):
+    def __inti__(self, __size=0):
         """ this function instantiate only positive integers"""
 
-        if type(size) is not int:
+        if not isinstance(__size, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if __size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
