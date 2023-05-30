28b725642876485830bded8a0d330e03c4425a4e
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-05-30 06:55:01 -0700

    a class Square that defines a squarewq

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 """This module defines a square class"""
 
-class Square:
-   """Defines a square"""
 
+class Square:
+    """Defines a square"""
     def __init__(self, size):
         """The function initializes a private attribute size"""
-
         self.__size = size
