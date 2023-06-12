e4370080517af8bc37a9b63ee152bc9123c6ad22
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-06-12 16:44:28 -0700

    all tasks

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+defined BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """create class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__widht = width
+        self.__height = height
