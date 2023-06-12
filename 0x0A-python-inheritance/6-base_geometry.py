47ff11e301823c7fe61ad742e900e932674527d0
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-06-12 16:22:17 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+defined BaseGeometry
+"""
+
+
+class BaseGeometry(object):
+    """
+    empty class
+    """
+
+    def area(self):
+        """
+        defined area method
+        Args: None
+        """
+        raise Exception("area() is not implemented") 
