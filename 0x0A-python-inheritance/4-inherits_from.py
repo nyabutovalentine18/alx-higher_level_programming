de84de68c5facd23f63eb649fc228ba43e3152df
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-06-12 16:12:32 -0700

    task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,5 +10,5 @@ def inherits_from(obj, a_class):
     """
 
     if issubclass(type(obj), a_class) is True and type(obj) != a_class:
-        return(True)
-    return(False)
+        return (True)
+    return (False)
