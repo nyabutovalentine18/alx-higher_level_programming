e5711c15f73107ddbd568b712eaa218116b0cc59
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-06-13 13:59:11 -0700

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+defined module
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object represented by a JSON string
+    """
+ 
+    return (json.loads(my_str))
