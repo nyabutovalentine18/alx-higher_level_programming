691e1e9410e787bd24f6deb223494bac2d8a34dd
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-07-18 07:10:17 -0700

    task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- display top 3 cities
+-- in descending order
+SELECT city , AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
