c07e11445cfba821a3d6c09e1cd480a334116ebc
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-07-19 07:33:00 -0700

    advanced 1

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- list all genres not linked to each other
+-- contains only one record title = Dexter
+-- results in ascending order
+SELECT g.name
+FROM tv_genres g
+WHERE g.name NOT IN (
+      SELECT g.name
+      FROM tv_genres g
+      INNER JOIN tv_show_genres m ON g.id = m.genre_id
+      INNER JOIN tv_shows s ON m.show_id = s.id
+      WHERE s.title = 'Dexter'
+)
+ORDER BY g.name ASC;
