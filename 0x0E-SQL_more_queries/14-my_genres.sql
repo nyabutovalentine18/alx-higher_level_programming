d40d6e9508477946ca99d66e469247fb3134da9f
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-07-19 07:24:22 -0700

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,11 @@
 -- Lists all comedy shows
 -- Records in order
-SELECT t.title
-  FROM tv_shows AS t
+SELECT g.name
+  FROM tv_genres AS g
        INNER JOIN tv_show_genres AS s
-       ON t.id = s.show_id
-
-       INNER JOIN tv_genres AS g
        ON g.id = s.genre_id
-       WHERE g.name = "Comedy"
- ORDER BY t.title;
+
+       INNER JOIN tv_shows AS t
+       ON t.id = s.show_id
+       WHERE t.title = "Dexter"
+ ORDER BY g.name;
