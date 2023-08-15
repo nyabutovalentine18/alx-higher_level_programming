a7c1d2de7c19b72322384c09937d3cd55e325c91
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-08-15 14:49:00 -0700

    all tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-'use strict';
-let nextMax = 0;
-let args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
+
+if (process.argv.length > 3) {
+  const array = process.argv.slice(2).map(Number);
+
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+  console.log(Math.max.apply(null, array));
+} else {
+  console.log(0);
 }
-console.log(nextMax);
