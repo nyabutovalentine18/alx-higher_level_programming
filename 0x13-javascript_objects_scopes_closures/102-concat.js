333812a4f7a3ba8228895bd548b757cedbfcbcd4
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-08-15 14:10:15 -0700

    all

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs').promises;
-const { argv } = require('process');
+const fs = require('fs');
 
-fs.readFile(argv[2], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, 'utf8'))
-  .catch(err => console.error(err));
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
 
-fs.readFile(argv[3], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
-  .catch(err => console.error(err));
+fs.writeFileSync(process.argv[4], fileA + fileB);
